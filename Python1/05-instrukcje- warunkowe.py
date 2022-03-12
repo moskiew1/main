919240b6a91cf355ad10fc7a6f334c650cefe40b
moskiew1 <moskwaradoslaw1@gmail.com>
2022-03-12 11:42:08 +0100

    aktualizacja 12.03

---
+++ Python1/05-instrukcje- warunkowe.py
@@ -0,0 +1,13 @@
+light = input("Jakie jest światło? Wpisz: (red, green, yellow)")
+if light == 'red' or "Red":
+    print("Czekaj!")
+elif light == 'yellow' or "Yellow":
+    print("Przygotuj się!")
+elif light =='green' or "Green":
+    print("Jedź!")    
+else:
+    print("Nie właściwa wartość")    
+
+#komentarz lub
+
+#  print("Jedź!") if light == 'green' else print("Czekaj!")
