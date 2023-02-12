12048299bae1cbfebc1c89c7ac00ff2ce97b8a94
moskiew1 <moskwaradoslaw1@gmail.com>
2023-02-12 20:55:19 +0100

    New kurs

--- Node.JS/kursNode/aplikacja/app/index.js
+++
@@ -1,6 +0,0 @@
-const app = require('./app.js')
-const { port } = require('./config')
-
-app.listen(port, () => console.log(`Serwer słucha na porcie: ${port}!`))
-
-
--- Node.JS/kursNode/aplikacja/cypress/e2e/apka/apka.cy.js
+++
@@ -1,6 +0,0 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('/')
-    cy.get(`[data-ctx=registration]`).click()
-  })
-})
