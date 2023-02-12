a5606baa54feea8a9f2a338d6b4059c367dd3f3d
moskiew1 <moskwaradoslaw1@gmail.com>
2023-02-12 10:28:17 +0100

    Update App node.js Api - api token

---
+++ Node.JS/kursNode/aplikacja/app/controllers/api/user-controller.js
@@ -0,0 +1,25 @@
+const User = require("../../db/models/user");
+
+class UserController {
+    async login(req, res) {
+        try {
+            const user = await User.findOne({email: req.body.email });
+            if (!user) 
+            {
+                throw new Error('user not found');
+            }
+
+            const isValidPassword = user.comparePassword(req.body.password); 
+            if(!isValidPassword)
+            {
+                throw new Error('password is not valid');
+            }
+
+            res.status(200).json({ apiToken: user.apiToken });
+        } catch (e) {
+            res.status(401)
+        }
+    }
+}
+
+module.exports = new UserController();
--- Node.JS/kursNode/aplikacja/app/routes/api.js
+++ Node.JS/kursNode/aplikacja/app/routes/api.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = new express.Router();
 const CompanyController = require('../controllers/api/company-controller');
+const UserController = require('../controllers/api/user-controller');
 const upload = require('../services/uploader');
 const authMiddleware = require('../middleware/is-auth-api-middleware');
 
@@ -8,5 +9,7 @@ router.get('/companies', CompanyController.showCompanies);
 router.post('/companies', authMiddleware, CompanyController.create);
 router.put('/companies/:slug', authMiddleware, upload.single('image'), CompanyController.edit);
 router.delete('/companies/:slug', authMiddleware, CompanyController.delete);
+
+router.post('/login', UserController.login);
 
 module.exports = router;
