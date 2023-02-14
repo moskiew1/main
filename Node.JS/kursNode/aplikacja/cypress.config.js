34b77ffdc72d3e51516ce31c15490ddf6d443e92
moskiew1 <moskwaradoslaw1@gmail.com>
2023-02-14 06:38:15 +0100

    Update Node.app Cy-test-homepage

--- Node.JS/kursNode/aplikacja/cypress.config.js
+++ Node.JS/kursNode/aplikacja/cypress.config.js
@@ -1,10 +1,13 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  includeShadowDom: true,
+  chromWebSecurity: true,
+  viewportHeight: 1080,
+  viewportWidth: 1920,
+  
+
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-    baseUrl:"http://localhost:3000"
+    baseUrl: 'http://localhost:3000'
   },
 });
--- Node.JS/kursNode/aplikacja/cypress/e2e/OpenHomePage.cy.js
+++ Node.JS/kursNode/aplikacja/cypress/e2e/OpenHomePage.cy.js
@@ -1,6 +1,5 @@
 describe('template spec', () => {
   it('passes', () => {
     cy.visit('/')
-    cy.get(`[data-ctx=registration]`).click()
   })
 })
